2bdbb7eb9f53b019eca94d370b1e620963f54e46
Chris Aporta <christopher.aporta@gmail.com>
2015-09-23 23:35:09 -0400

    complete lab

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  sorted_hash = {}
+  data.each do |key, value|
+    value.each do |k, v|
+      v.each do |name|
+        if !sorted_hash.keys.include?(name)
+          sorted_hash[name] = {}
+        end
+        sorted_hash[name][key] = []
+      end
+    end
+  end
+  sorted_hash.each do |key, value|
+    data.each do |k, v|
+      v.each do |kk, vv|
+        if vv.include?(key)
+          sorted_hash[key][k] << kk.to_s
+        end
+      end
+    end
+  end
+  sorted_hash
 end
